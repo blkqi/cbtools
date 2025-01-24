45c48ff24f7c4fd20326bfe3b51b4a70a71cf860
Brett Kleinschmidt <blk@blk.me>
2025-01-24 12:04:25 -0800

    Add packaging

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='cbtools',
+      version='0.0.0',
+      package_dir={'': 'src'},
+      packages=['cbtools'],
+      scripts=['bin/cbrename'],
+      install_requires=[])
