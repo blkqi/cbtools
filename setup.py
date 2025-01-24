d29ac44b87108af37c68dd29f62661be17e4c676
Brett Kleinschmidt <blk@blk.me>
2025-01-24 12:29:01 -0800

    Add bin/cbtag

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='cbtools',
       version='0.0.0',
       package_dir={'': 'src'},
       packages=['cbtools'],
-      scripts=['bin/cbinfo', 'bin/cbrename'],
+      scripts=['bin/cbinfo', 'bin/cbrename', 'bin/cbtag'],
       install_requires=['lxml'])
