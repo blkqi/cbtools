234e9be93bd2fb11d25860e0d12501651fa263bd
Brett Kleinschmidt <blk@blk.me>
2025-01-24 12:17:34 -0800

    Add cbinfo utility

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='cbtools',
       version='0.0.0',
       package_dir={'': 'src'},
       packages=['cbtools'],
-      scripts=['bin/cbrename'],
+      scripts=['bin/cbinfo', 'bin/cbrename'],
       install_requires=['lxml'])
