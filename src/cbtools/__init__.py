2eda469127dda80c34edb79756660015a3493252
blk <dev+github@cloak.blk.me>
2025-01-27 14:17:01 -0800

    Add tagger utility: cbtag (#12)
    * Add bin/cbtag
    
    * Map anilist API data to comicinfo (#5)
    
    * Map anilist API data to comicinfo
    
    * Fix for publisher - array to str
    
    ---------
    
    Co-authored-by: ckleinschmidt <ckleinschmidt@unicef.org>
    
    * Handle null response from anilist
    
    * Use ComicInfo
    
    * Implement ComicInfo.encode
    
    * Use --dryrun to display a suggested change
    
    * Use ComicInfo
    
    * Use --dryrun to display a suggested change
    
    * Let argparse impose pathlib.Path
    
    * Remove debug comments
    
    * Remove debug comments
    
    * Revert "Remove debug comments"
    
    This reverts commit dd0a059a9e3ae29881619816c5290b5f3026b7a5.
    
    * Remove validate on parse option
    
    * Default encoding pretty
    
    * Implement ComicInfo.xml replacement
    
    * Improve implementation
    
    * Parse volume
    
    * Parse volume, fix path handling on windows
    
    * Fix vol parsing when writing tags
    
    * Indentation
    
    * cbtag refactors
    
    * Ws
    
    * Remove info()
    
    * Update cbinfo and cbrename
    
    * Move compare method
    
    * Move replace method
    
    * Read GQL from package data
    
    * Get schema from package data
    
    * Do not set null volume
    
    * Rename --series-id to --anilist
    
    * Skip cinfo writing if not diff
    
    * Move windows path expansion to cbtools
    
    * Use expand_paths in all modules
    
    ---------
    
    Co-authored-by: Brett Kleinschmidt <blk@blk.me>
    Co-authored-by: Cory <778005+corylk@users.noreply.github.com>
    Co-authored-by: ckleinschmidt <ckleinschmidt@unicef.org>

--- src/cbtools/__init__.py
+++ src/cbtools/__init__.py
@@ -1,39 +1,102 @@
-import zipfile
+import dictdiffer
+import jmespath
 import lxml.etree
+import pathlib
+import platform
+import re
+import requests
+import shutil
+import tempfile
+import zipfile
+import importlib.resources
+
+from glob import glob
+
+class AniList:
+    def __init__(self):
+        self.api_url = 'https://graphql.anilist.co'
+
+    def search(self, series_id):
+        media_format = 'MANGA'
+        query = importlib.resources.files(__name__).joinpath('anilistid.gql').open().read()
+        variables = {
+            'series_id': series_id,
+            'format': media_format
+        }
+        response = requests.post(self.api_url, json={'query': query, 'variables': variables})
+        return AniListResponse(response.json())
+
+class AniListResponse(dict):
+    ANILIST_COMICINFO_JMESMAP = {
+        'Series': 'title.romaji',
+        'LocalizedSeries': 'title.english',
+        'Count': 'volumes',
+        'Writer': 'staff.edges[?role.contains(@, `Story`)].node.name.full | [0]',
+        'Penciller': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
+        'Inker': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
+        'Colorist': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
+        'CoverArtist': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
+        'Publisher': 'studios.edges[?isMain==true].node.name | [0]',
+        'Genre': 'genres[*] | join(`,`, @)',
+        'Tags': 'tags[*].name | join(`,`, @)',
+        'Summary': 'description',
+        'Web': 'siteUrl',
+        'Year': 'startDate.year',
+        'Month': 'startDate.month',
+        'Day': 'startDate.day',
+    }
+
+    def to_cinfo(self):
+        cinfo = ComicInfo()
+        data = self.get('data', {}).get('Media', {})
+
+        if not data:
+            return cinfo
+
+        cinfo.map(data, self.ANILIST_COMICINFO_JMESMAP)
+
+        return cinfo
 
 class ComicInfo(dict):
-    XSD_FILENAME = 'ComicInfo.xsd'
+    XSD_FILENAME = importlib.resources.files(__name__).joinpath('ComicInfo.xsd')
     XML_FILENAME = 'ComicInfo.xml'
 
     def __init__(self, *args, **kwds):
         super(ComicInfo, self).__init__(*args, **kwds)
 
-    def parse(f, validate=False):
-        xml_tree = lxml.etree.parse(f)
+    def parse(f):
+        tree = lxml.etree.parse(f)
 
-        # TODO allow injecting default values into validated documents
-        if validate:
-            ComicInfo.validate(xml_tree)
+        return ComicInfo((child.tag, child.text) for child in tree.getroot())
 
-        return ComicInfo((child.tag, child.text) for child in xml_tree.getroot())
+    def encode(self, pretty_print=True, **kwds):
+        root = lxml.etree.Element('ComicInfo')
 
-    def validate(xml_tree):
+        for name, value in self.items():
+            lxml.etree.SubElement(root, name).text = str(value or '')
+
+        return lxml.etree.tostring(root, pretty_print=pretty_print, **kwds)
+
+    def validate(tree):
         xsd_tree = lxml.etree.parse(ComicInfo.XSD_FILENAME)
-        schema = lxml.etree.XMLSchema(xsd_tree)
-        schema.assertValid(xml_tree)
+        lxml.etree.XMLSchema(xsd_tree).assertValid(tree)
+
+    def compare(self, with_data):
+        result = dictdiffer.diff(self, with_data)
+        return list(result)
+
+    def map(self, source, jmesmap):
+        for target_key, source_key in jmesmap.items():
+            value = jmespath.search(source_key, source)
+
+            if value:
+                self[target_key] = str(value)
 
 class CBZFile(zipfile.ZipFile):
     def __init__(self, file, **kwds):
         super(CBZFile, self).__init__(file, **kwds)
-
-    def info(self, validate=False):
-        try:
-            with self.open(ComicInfo.XML_FILENAME) as c:
-                return ComicInfo.parse(c, validate=validate)
-        except KeyError:
-            pass
-
-        return ComicInfo()
+        self.info = self._get_info()
+        self.volume = self._parse_volume()
 
     def extractall(self, path=None, members=None, pwd=None, flatten=False):
         if not flatten:
@@ -46,3 +109,43 @@ class CBZFile(zipfile.ZipFile):
             member.filename = member.filename.replace('/', '__')
 
             self.extract(member, path)
+
+    def update_cinfo(self, cinfo):
+        with tempfile.TemporaryDirectory() as tempdir:
+            temppath = pathlib.Path(tempdir) / 'cbz'
+
+            with CBZFile(temppath, mode='w') as cbzwrite:
+                cbzwrite.writestr(ComicInfo.XML_FILENAME, cinfo.encode())
+
+                for item in self.infolist():
+                    if item.filename != ComicInfo.XML_FILENAME:
+                        data = self.read(item.filename)
+                        cbzwrite.writestr(item, data)
+
+            shutil.copyfile(temppath, self.filename)
+            temppath.unlink()
+
+    def _get_info(self):
+        try:
+            with self.open(ComicInfo.XML_FILENAME) as c:
+                return ComicInfo.parse(c)
+        except KeyError:
+            pass
+
+        return ComicInfo()
+
+    def _parse_volume(self):
+        filename_parts = self.filename.split(' ')
+        filename_parts.reverse()
+
+        for part in filename_parts:
+            found = re.search(r'^[vV]{1}\d+$', part)
+
+            if found:
+                return str(int(found.group(0)[1:]))
+
+def expand_paths(paths):
+    if platform.system() != 'Windows':
+        return paths
+
+    return [f for e in paths for f in glob(str(e))]
