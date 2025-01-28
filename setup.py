d24f63c8427b35159bb19bf4a1243d7c4d859588
Brett Kleinschmidt <blk@blk.me>
2025-01-28 07:23:33 -0800

    Add distribution licensing

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(name='cbtools',
       package_data={'cbtools': ['*.xsd', '*.gql']},
       packages=['cbtools'],
       scripts=['bin/cbinfo', 'bin/cbrename', 'bin/cbtag', 'bin/cbscale'],
-      install_requires=['lxml', 'jmespath', 'requests', 'dictdiffer'],)
+      install_requires=['lxml', 'jmespath', 'requests', 'dictdiffer'],
+      license_files=['LICENSE'],)
