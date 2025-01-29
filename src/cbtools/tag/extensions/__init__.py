156cfadbc5a4d04520dc2dc1a271c0b51150065b
ckleinschmidt <ckleinschmidt@unicef.org>
2025-01-29 06:39:20 -0500

    Count extension

--- src/cbtools/tag/extensions/__init__.py
+++ src/cbtools/tag/extensions/__init__.py
@@ -1,3 +1,4 @@
 from cbtools.tag.extensions import tag_highly_rated
 
-__all__ = ['tag_highly_rated', 'tag_adult', 'manga_yes', 'community_rating', 'notes_cbtag']
+__all__ = ['tag_highly_rated', 'tag_adult', 'manga_yes',
+           'community_rating', 'notes_cbtag', 'count_volumes']
