9cd2a2c57ecc1a8f8b1059d78170adcbb85c0f61
ckleinschmidt <ckleinschmidt@unicef.org>
2025-01-29 07:47:16 -0500

    Fix handle null avg score

--- src/cbtools/tag/extensions/tag_highly_rated.py
+++ src/cbtools/tag/extensions/tag_highly_rated.py
@@ -1,5 +1,5 @@
 # This extension adds the tag 'Highly Rated' to the comic if the average score is 85 or higher.
 
 def extension(cinfo, data):
-    if data.get('averageScore', 0) >= 85:
+    if (data.get('averageScore') or 0) >= 85:
         cinfo['Tags'] = ','.join([cinfo['Tags'], 'Highly Rated'])
