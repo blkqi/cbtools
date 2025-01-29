f784d6d30fdcf7fa1d79639d9fbb9819b85259e1
ckleinschmidt <ckleinschmidt@unicef.org>
2025-01-29 06:55:58 -0500

    Remove count vols

--- src/cbtools/tag/extensions/__init__.py
+++ src/cbtools/tag/extensions/__init__.py
@@ -1,4 +1,3 @@
 from cbtools.tag.extensions import tag_highly_rated
 
-__all__ = ['tag_highly_rated', 'tag_adult', 'manga_yes',
-           'community_rating', 'notes_cbtag', 'count_volumes']
+__all__ = ['tag_highly_rated', 'tag_adult', 'manga_yes', 'community_rating', 'notes_cbtag']
--- src/cbtools/tag/extensions/count_volumes.py
+++
@@ -1,5 +0,0 @@
-# This extension sets the 'Count' field to the number of volumes if the series is finished.
-
-def extension(cinfo, data):
-    if data['status'].upper() == 'FINISHED' and data.get('volumes'):
-        cinfo['Count'] = str(data['volumes'])
