54766c1ae78ce45c06443dace1c1fb728424208a
Brett Kleinschmidt <blk@blk.me>
2025-01-30 22:01:13 -0800

    Rename methods

--- src/cbtools/rename/__init__.py
+++ src/cbtools/rename/__init__.py
@@ -1,47 +1,58 @@
 import os
 import shutil
+import string
 import pathlib
 import logging
 
-from string import Template
 from cbtools.config import config
 from cbtools.core import CBZFile, expand_paths
+from operator import itemgetter
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
-ARG_PATTERN_DEFAULT = '${Series} (${Year})/${Series} ${Volume}'
-FILENAME_SUFFIX = '.cbz'
-ALLOWED_CHARS = string.ascii_letters + string.digits + " _-~.'!@#$%^&()[]{}"
+__allowed_chars = string.ascii_letters + string.digits + " _-~.'!@#$%^&()[]{}"
+__formatters = {'Series': str,
+                'Volume': lambda x: f'V{int(x.split('.')[0]):02}{f'.{x.split('.')[-1]}' if '.' in x else ''}',
+                'Writer': str,
+                'Year':   str}
 
-def cbrename_format(cinfo):
-    formatters = {'Series': str,
-                  'Volume': lambda x: f'V{int(x.split('.')[0]):02}{f'.{x.split('.')[-1]}' if '.' in x else ''}',
-                  'Writer': str,
-                  'Year':   str}
+def sanitize_paths(value):
+    return ''.join(c for c in value if c in __allowed_chars)
+
+def format_cinfo(cfile, pattern, default=''):
+    cinfo = cfile.info
 
     # prefer localized series to series
     cinfo['Series'] = cinfo.get('LocalizedSeries') or cinfo.get('Series')
 
-    mapval = lambda k, f: cbrename_sanitize(f(cinfo.get(k)))
-    return {k: mapval(k, f) for k, f in formatters.items() if k in cinfo}
-
-def cbrename_substitute(cfile, pattern, default=''):
-    template = Template(pattern)
+    template = string.Template(pattern)
     defaults = {key: default for key in template.get_identifiers()}
+    values = {k: sanitize_paths(f(cinfo[k])) for k, f in __formatters.items() if k in cinfo}
+    strpath = template.substitute(defaults, **values)
 
-    s = template.substitute(defaults, **cbrename_format(cfile.info))
-    return pathlib.Path(s.strip())
+    return pathlib.Path(strpath.strip())
 
-def cbrename_sanitize(value):
-    return ''.join(c for c in value if c in ALLOWED_CHARS)
-
-def cbrename_path(cfile, pattern, *, root):
-    stem = cbrename_substitute(cfile, pattern)
+def determine_path(cfile, *, root, pattern=config['rename_pattern']):
+    stem = format_cinfo(cfile, pattern)
     suffix = cfile.Path().suffix
     return (root / stem).with_suffix(suffix)
 
-def cbrename_rename(src, dst):
+def determine_pairs(paths, *, root):
+    for src in paths:
+        with CBZFile(src) as cfile:
+            dst = determine_path(cfile, root=root)
+            if src != dst:
+                yield src, dst
+
+def determine_extra(pairs, includes=()):
+    parents = set((src.parent, dst.parent) for src, dst in pairs)
+    for inc in includes:
+        for src, dst in parents:
+            if (src / inc).exists():
+                yield (src / inc, dst / inc)
+
+def rename_file(src, dst):
     dst.parent.mkdir(parents=True, exist_ok=True)
 
     try:
@@ -58,37 +69,23 @@ def cbrename_rename(src, dst):
     shutil.copyfile(src, dst)
     src.unlink()
 
-def cbrename_pairs(paths, pattern, **kwds):
-    for src in paths:
-        with CBZFile(src) as cfile:
-            dst = cbrename_path(cfile, pattern, **kwds)
-            if src != dst:
-                yield src, dst
-
-def cbrename_extra(pairs, includes=()):
-    parents = set((src.parent, dst.parent) for src, dst in pairs)
-    for inc in includes:
-        for src, dst in parents:
-            if (src / inc).exists():
-                yield (src / inc, dst / inc)
-
-def cbrename(files, pattern=ARG_PATTERN_DEFAULT, validate=False, dryrun=False, **kwds):
+def cbrename(files, *, root, validate=False, dryrun=False):
     paths = expand_paths(files)
-    pairs = set(cbrename_pairs(paths, pattern, **kwds))
+    pairs = set(determine_pairs(paths, root=root))
 
     for src, _ in pairs:
         if not src.exists():
             raise FileNotFoundError('file "{src}" does not exist!')
         # TODO detect or prevent collisions and overwrites
 
-    extra = set(cbrename_extra(pairs, includes=config['move_includes']))
+    extra = set(determine_extra(pairs, includes=config['move_includes']))
     union = pairs.union(extra)
 
-    for src, dst in sorted(union):
+    for src, dst in sorted(union, key=itemgetter(1)):
         if dryrun:
             print(f'(dryrun) "{src}" -> "{dst}"')
         else:
-            cbrename_rename(src, dst)
+            rename_file(src, dst)
 
     try:
         next(src.parent.iterdir())
