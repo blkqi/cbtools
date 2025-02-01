c8f687fb234dbf4021c2689b6d009b67756a8226
ckleinschmidt <ckleinschmidt@unicef.org>
2025-02-01 09:41:52 -0500

    Fix setup and add list and clear endpoitns to cbmanager

--- src/cbtools/manager/queue/__init__.py
+++ src/cbtools/manager/queue/__init__.py
@@ -9,32 +9,38 @@ class ManagerQueueItem:
 
 class ManagerQueue:
     def __init__(self, delay: float) -> None:
-        self.queue: List[ManagerQueueItem] = []
-        self.delay = delay
+        self._queue: List[ManagerQueueItem] = []
+        self._delay = delay
 
     def enqueue(self, item: Any) -> None:
         if self._is_queued(item):
             return
 
-        self.queue.append(ManagerQueueItem(item))
+        self._queue.append(ManagerQueueItem(item))
 
     def dequeue(self) -> Optional[Any]:
         if self._is_next_ready():
-            return self.queue.pop(0).item
+            return self._queue.pop(0).item
 
         return None
 
+    def list_items(self) -> List[str]:
+        return [str(q.item) for q in self._queue]
+
+    def clear(self) -> None:
+        self._queue = []
+
     def flush(self) -> None:
-        for item in self.queue:
+        for item in self._queue:
             item.time = 0
 
     def _is_queued(self, item: Any) -> bool:
-        return item in [q.item for q in self.queue]
+        return item in [q.item for q in self._queue]
 
     def _is_next_ready(self) -> bool:
-        return not self._is_empty() and time.time() - self.queue[0].time >= self.delay
+        return not self._is_empty() and time.time() - self._queue[0].time >= self._delay
 
     def _is_empty(self) -> bool:
-        return len(self.queue) == 0
+        return len(self._queue) == 0
 
 manager_queue = ManagerQueue(300)
