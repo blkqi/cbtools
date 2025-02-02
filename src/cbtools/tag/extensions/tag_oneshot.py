5c44cb4e2735b7497709913ee8fe8e1c1dd8a274
ckleinschmidt <ckleinschmidt@unicef.org>
2025-02-02 18:15:24 -0500

    Add oneshot tag extension

---
+++ src/cbtools/tag/extensions/tag_oneshot.py
@@ -0,0 +1,7 @@
+from typing import Dict, Any
+
+# This extension adds the tag 'Oneshot' to the comic if the comic has only one volume and is finished.
+
+def extension(cinfo: Dict[str, Any], data: Dict[str, Any]) -> None:
+    if data['volumes'] == 1 and data['status'] == 'FINISHED':
+        cinfo['Tags'] = 'Oneshot'
