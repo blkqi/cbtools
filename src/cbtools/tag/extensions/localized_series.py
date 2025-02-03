e2d7b44c2fbe41835a39d4ffa49a5362ec3ea570
ckleinschmidt <ckleinschmidt@unicef.org>
2025-02-03 09:35:30 -0500

    Compare case insensitive

--- src/cbtools/tag/extensions/localized_series.py
+++ src/cbtools/tag/extensions/localized_series.py
@@ -3,8 +3,8 @@ from typing import Dict, Any
 # This extension sets the 'LocalizedSeries' to the 'english' title if different than the 'romaji' title
 
 def extension(cinfo: Dict[str, Any], data: Dict[str, Any]) -> None:
-    romanji_title = data.get('title', {}).get('romaji')
-    english_title = data.get('title', {}).get('english')
+    romanji_title = data.get('title', {}).get('romaji') or ''
+    english_title = data.get('title', {}).get('english') or ''
 
-    if english_title and english_title != romanji_title:
+    if english_title and english_title.lower() != romanji_title.lower():
         cinfo['LocalizedSeries'] = english_title
