3f86fafc39508982b9e37d23322db02475e591d4
Cory <778005+corylk@users.noreply.github.com>
2025-02-03 11:22:47 -0500

    No complex types in config object (#47)
    Co-authored-by: ckleinschmidt <ckleinschmidt@unicef.org>

--- src/cbtools/__init__.py
+++ src/cbtools/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import pathlib
 
 from logging.handlers import TimedRotatingFileHandler
 
@@ -16,11 +17,13 @@ def configure_logging() -> logging.Logger:
     return logger
 
 def configure_file_logging(name: str) -> None:
-    if not config['logging.path'].exists():
-        config['logging.path'].mkdir(exist_ok=True)
+    log_path = pathlib.Path(config['logging.path'])
+
+    if not log_path.exists():
+        log_path.mkdir(exist_ok=True)
 
     handler = TimedRotatingFileHandler(
-        filename=config['logging.path'] / f'{name}.log',
+        filename=log_path / f'{name}.log',
         when='D',
         interval=1,
         backupCount=6,
