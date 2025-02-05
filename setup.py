e2ecbed21454245ea4c72e05cb9d14db9dc75c8b
Brett Kleinschmidt <blk@blk.me>
2025-02-04 20:25:58 -0800

    Rearrange source tree to reduce module

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup
 
+packages=[
+    'cbtools',
+    'cbtools.core',
+    'cbtools.manager',
+    'cbtools.tag',
+    'cbtools.tag.extensions'
+]
+
 setup(name='cbtools',
       version='0.0.0',
       package_dir={'': 'src'},
       package_data={'cbtools': ['*.xsd'], 'cbtools.tag': ['*.gql']},
-      packages=['cbtools',
-                'cbtools.config',
-                'cbtools.core',
-                'cbtools.core.constants',
-                'cbtools.manager',
-                'cbtools.manager.api',
-                'cbtools.manager.queue',
-                'cbtools.rename',
-                'cbtools.tag',
-                'cbtools.tag.extensions'],
+      packages=packages,
       scripts=['bin/cbinfo', 'bin/cbrename', 'bin/cbtag', 'bin/cbscale', 'bin/cbmanager'],
       install_requires=['lxml', 'jmespath', 'requests', 'dictdiffer', 'waitress', 'flask', 'watchdog'],
       license_files=['LICENSE'],)
