96207d5a36b608f87e65b8df5280468cf9d460e6
Brett Kleinschmidt <blk@blk.me>
2025-02-08 19:57:41 -0800

    Remove KCC

--- src/cbtools/convert.py
+++ src/cbtools/convert.py
@@ -1,41 +1,86 @@
-import logging
 import typing
-import copy
-import kindlecomicconverter.comic2ebook as kcc
+import tempfile
+import itertools
 
-from types import SimpleNamespace
+from pathlib import Path
+from cbtools import image
+from cbtools.log import logger
+from cbtools.core import ComicArchive, ComicInfo
+from cbtools.constants import *
 
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
+
+def _write_output(outpath, srcpath, dstpath):
+    logger.info(f'Packing archive "{outpath}"')
+
+    cfile = ComicArchive(outpath)
+
+    add_paths = sorted(dstpath.iterdir())
+    info_path = srcpath / COMICINFO_XML_NAME
+
+    if info_path.exists():
+        add_paths = itertools.chain([info_path], add_paths)
+
+    for path in add_paths:
+        with open(path, 'rb') as f:
+            cfile.add(path.name, f)
+
+    logger.info(f'Created archive "{outpath}"')
+
+
+def _convert_images(srcpath, dstpath, profile, flags):
+    for path in sorted(srcpath.iterdir()):
+        target = (dstpath / path.name)
+        try:
+            image.convertImage(str(path), str(target), 'Kindle Scribe', flags)
+        except RuntimeError as e:
+            logger.warning(f'Cannot read image file {path}')
+            pass
+        # TODO auto contrast ?
+
+
+def _extract_flatten(cfile, root):
+    logger.info(f'Unpacking archive "{cfile.filepath}"')
+
+    for member in cfile.list():
+        if member.is_dir():
+            continue
+
+        name = Path(member.name).as_posix().replace('/', '__')
+        path = (root / name)
+
+        with open(path, 'wb') as f:
+            cfile.extract(member.name, f)
+
+
+_output_suffix = 'cbconvert'
+
+def _output_filename(path, root=None):
+    stem = '_'.join((path.stem, _output_suffix))
+    return ((root or path.parent) / (str(stem) + '.cbz'))
+
 
 def convert(path, **kwds):
-    kcc.options = SimpleNamespace(
-        input = [],
-        format = 'CBZ',
-        title = 'defaulttitle',
-        author = 'defaultauthor',
-        delete = False,
-        noprocessing = False,
-        white_borders = False,
-        black_borders = False,
-        maximizestrips = False,
-        hq = False,
-        interpanelcrop = False,
-        stretch = False,
-        webtoon = False,
-        forcepng = False,
-        forcecolor = False,
-        mozjpeg = False,
-        upscale = True, # Resize images to device's resolution
-        gamma = 0.0, # Automatic gamma correction
-        splitter = 2, # Rotate spreads
-        cropping = 0, # Disable cropping
-        batchsplit = 0,
-        customwidth = 0,
-        customheight = 0,
-        righttoleft = False,
-        norotate = False,
-        **kwds,
-    )
-    kcc.checkOptions(kcc.options)
-    kcc.makeBook(str(path))
+    cfile = ComicArchive(path)
+
+    ## image processing options
+    flags = 0
+    flags |= image.ImageFlags.Resize
+    #flags |= image.ImageFlags.Fill
+    #flags |= image.ImageFlags.Quantize
+
+    ## device profile
+    profile = 'Kindle Scribe'
+
+    with tempfile.TemporaryDirectory() as srcdir, \
+            tempfile.TemporaryDirectory() as dstdir:
+
+        src_path = Path(srcdir)
+        dst_path = Path(dstdir)
+        out_path = _output_filename(cfile.filepath, root=path.parent)
+
+        if out_path.exists():
+            raise RuntimeError(f'path "{out_path}" already exists!')
+
+        _extract_flatten(cfile, src_path)
+        _convert_images(srcpath, dst_path, profile, flags)
+        _write_output(out_path, src_path, dst_path)
