cd9adac07648c74b605feb2f10e54488ca01c5b8
Brett Kleinschmidt <blk@blk.me>
2025-02-13 08:33:13 -0800

    Factor out anilist module

--- src/cbtools/tag/__init__.py
+++ src/cbtools/tag/__init__.py
@@ -1,117 +1,9 @@
-import importlib.resources
-import jmespath
-import requests
-import time
-import logging
-
-from pathlib import Path
-
 from cbtools.log import logger
-from cbtools.core import ComicInfo, ComicArchive, expand_paths
+from cbtools.core import ComicArchive, expand_paths
 from cbtools.config import config
 from cbtools.constants import COMICINFO_XML_NAME
+from cbtools.tag.anilist import AniList
 
-class AniListAdapter(requests.adapters.HTTPAdapter):
-    def __init__(self, *args, **kwds):
-        super().__init__(*args, **kwds)
-
-    def send(self, request, **kwds):
-        while True:
-            response = super().send(request, **kwds)
-
-            if not self._throttle(response):
-                break
-
-        return response
-
-    def _throttle(self, response):
-        if response.status_code == 429:
-            self._wait(int(response.headers['Retry-After']))
-            return True
-        else:
-            return False
-
-    def _wait(self, period):
-        logger.warn(f'AniList rate limit exceeded! Retry in {period} seconds.')
-        time.sleep(period)
-
-class AniList:
-    def __init__(self):
-        self.api_url = 'https://graphql.anilist.co'
-
-        adapter = AniListAdapter()
-        self.session = requests.session()
-        self.session.mount('http://', adapter)
-        self.session.mount('https://', adapter)
-
-    def search(self, series_id):
-        media_format = 'MANGA'
-        query = importlib.resources.files(__name__).joinpath('anilistid.gql').open().read()
-        variables = {
-            'series_id': series_id,
-            'format': media_format
-        }
-
-        response = self.session.post(self.api_url, json={'query': query, 'variables': variables})
-        response.raise_for_status()
-
-        return AniListResponse(response.json())
-
-class AniListResponse(dict):
-    ANILIST_COMICINFO_JMESMAP = {
-        'Series': 'title.romaji',
-        'Count': 'volumes',
-        'Writer': 'staff.edges[?role.contains(@, `Story`)].node.name.full | [0]',
-        'Penciller': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
-        'Inker': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
-        'Colorist': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
-        'CoverArtist': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
-        'Letterer': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
-        'Publisher': 'studios.edges[?isMain==true].node.name | [0]',
-        'Genre': 'genres[*] | join(`,`, @)',
-        'Tags': 'tags[*].name | join(`,`, @)',
-        'Summary': 'description',
-        'Web': 'siteUrl',
-        'Year': 'startDate.year',
-        'Month': 'startDate.month',
-        'Day': 'startDate.day',
-    }
-
-    @property
-    def media(self):
-        return self.get('data', {}).get('Media', {})
-
-    def to_cinfo(self):
-        cinfo = ComicInfo()
-        data = self.get('data', {}).get('Media', {})
-
-        if not data:
-            return cinfo
-
-        cinfo = self._map_cinfo(self.ANILIST_COMICINFO_JMESMAP, cinfo)
-
-        return cinfo
-
-    def _map_cinfo(self, jmesmap, cinfo=None):
-        cinfo = cinfo or ComicInfo()
-
-        for target_key, source_key in jmesmap.items():
-            value = jmespath.search(source_key, self.media)
-
-            if value:
-                cinfo[target_key] = str(value)
-
-        self._apply_extensions(cinfo)
-
-        return cinfo
-
-    def _apply_extensions(self, cinfo):
-        for extension in config['tag.extensions']:
-            try:
-                module = importlib.import_module(f'cbtools.tag.extensions.{extension}')
-                module.extension(cinfo, self.media)
-            except ImportError:
-                logger.error(f'Failed to import extension {extension}')
 
 def _get_series_id(path):
     if path.is_file():
@@ -122,6 +14,7 @@ def _get_series_id(path):
             return int(file.read().strip())
     except FileNotFoundError:
         return None
+
 
 def _write_series_id(path, series_id):
     if not config['tag.write_series_id_file']:
@@ -135,6 +28,7 @@ def _write_series_id(path, series_id):
     with open(series_id_file_path, 'w') as file:
         file.write(str(series_id))
 
+
 def _fetch_comic_info(client, path, series_id=None, dryrun=False):
 
     series_id = series_id or _get_series_id(path)
@@ -146,6 +40,7 @@ def _fetch_comic_info(client, path, series_id=None, dryrun=False):
         _write_series_id(path.parent, series_id)
 
     return client.search(series_id).to_cinfo()
+
 
 def _tag_comic(path, cinfo, dryrun=False):
     cfile = ComicArchive(path)
@@ -164,6 +59,7 @@ def _tag_comic(path, cinfo, dryrun=False):
             print(item)
     else:
         cfile.write(COMICINFO_XML_NAME, cinfo.encode())
+
 
 def tag(files, series_id=None, dryrun=False):
     client = AniList()
---
+++ src/cbtools/tag/anilist.py
@@ -0,0 +1,113 @@
+import time
+import requests
+import jmespath
+import importlib.resources
+
+from cbtools.log import logger
+from cbtools.core import ComicInfo
+from cbtools.config import config
+
+
+class AniListAdapter(requests.adapters.HTTPAdapter):
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+
+    def send(self, request, **kwds):
+        while True:
+            response = super().send(request, **kwds)
+
+            if not self._throttle(response):
+                break
+
+        return response
+
+    def _throttle(self, response):
+        if response.status_code == 429:
+            self._wait(int(response.headers['Retry-After']))
+            return True
+        else:
+            return False
+
+    def _wait(self, period):
+        logger.warn(f'AniList rate limit exceeded! Retry in {period} seconds.')
+        time.sleep(period)
+
+
+class AniList:
+    def __init__(self):
+        self.api_url = 'https://graphql.anilist.co'
+
+        adapter = AniListAdapter()
+        self.session = requests.session()
+        self.session.mount('http://', adapter)
+        self.session.mount('https://', adapter)
+
+    def search(self, series_id):
+        media_format = 'MANGA'
+        query = importlib.resources.files(__name__).joinpath('anilistid.gql').open().read()
+        variables = {
+            'series_id': series_id,
+            'format': media_format
+        }
+
+        response = self.session.post(self.api_url, json={'query': query, 'variables': variables})
+        response.raise_for_status()
+
+        return AniListResponse(response.json())
+
+
+class AniListResponse(dict):
+    ANILIST_COMICINFO_JMESMAP = {
+        'Series': 'title.romaji',
+        'Count': 'volumes',
+        'Writer': 'staff.edges[?role.contains(@, `Story`)].node.name.full | [0]',
+        'Penciller': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
+        'Inker': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
+        'Colorist': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
+        'CoverArtist': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
+        'Letterer': 'staff.edges[?role.contains(@, `Art`)].node.name.full | [0]',
+        'Publisher': 'studios.edges[?isMain==true].node.name | [0]',
+        'Genre': 'genres[*] | join(`,`, @)',
+        'Tags': 'tags[*].name | join(`,`, @)',
+        'Summary': 'description',
+        'Web': 'siteUrl',
+        'Year': 'startDate.year',
+        'Month': 'startDate.month',
+        'Day': 'startDate.day',
+    }
+
+    @property
+    def media(self):
+        return self.get('data', {}).get('Media', {})
+
+    def to_cinfo(self):
+        cinfo = ComicInfo()
+        data = self.get('data', {}).get('Media', {})
+
+        if not data:
+            return cinfo
+
+        cinfo = self._map_cinfo(self.ANILIST_COMICINFO_JMESMAP, cinfo)
+
+        return cinfo
+
+    def _map_cinfo(self, jmesmap, cinfo=None):
+        cinfo = cinfo or ComicInfo()
+
+        for target_key, source_key in jmesmap.items():
+            value = jmespath.search(source_key, self.media)
+
+            if value:
+                cinfo[target_key] = str(value)
+
+        self._apply_extensions(cinfo)
+
+        return cinfo
+
+    def _apply_extensions(self, cinfo):
+        for extension in config['tag.extensions']:
+            try:
+                module = importlib.import_module(f'cbtools.tag.extensions.{extension}')
+                module.extension(cinfo, self.media)
+            except ImportError:
+                logger.error(f'Failed to import extension {extension}')
