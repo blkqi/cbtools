8e2078de8f25868967432a01ec812371ded0fdc5
Brett Kleinschmidt <blk@blk.me>
2025-02-13 16:23:22 -0800

    fixes

--- src/cbtools/repack.py
+++ src/cbtools/repack.py
@@ -1,10 +1,11 @@
-from cbtools.core import ComicArchive
+from cbtools.core import ComicArchive, expand_paths
+from cbtools.log import logger
 
 
 _repack_file_type = '.cbz'
 
 def repack(files, **kwds):
-    for src_path in expand_files(files):
+    for src_path in expand_paths(files):
         dst_path = src_path.with_suffix(_repack_file_type)
 
         if dst_path.exists():
@@ -16,7 +17,7 @@ def repack(files, **kwds):
         src_cfile = ComicArchive(src_path)
         dst_cfile = ComicArchive(dst_path)
 
-        for member in src_cfile.members():
-            dst_cfile.write(member.name, src_cfile.read(member.arcname))
+        for member in src_cfile.list():
+            dst_cfile.write(member.name, src_cfile.read(member.name))
 
         logger.debug(f'repack complete: {src_path} -> {dst_path}')
