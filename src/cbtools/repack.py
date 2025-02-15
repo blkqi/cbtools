e34917c04530d2f43c921cb15f5a1fbb29a9abd7
Cory <778005+corylk@users.noreply.github.com>
2025-02-15 09:46:50 -0500

    Repack changes, add repack to manager (#67)
    * Repack - handle supported file types and skip cbzs
    
    * Add remove source and dry run
    
    ---------
    
    Co-authored-by: ckleinschmidt <ckleinschmidt@unicef.org>

--- src/cbtools/repack.py
+++ src/cbtools/repack.py
@@ -1,12 +1,24 @@
+from cbtools.constants import SUPPORTED_FILE_EXTENSIONS
 from cbtools.core import ComicArchive, expand_paths
 from cbtools.log import logger
 
 
 _repack_file_type = '.cbz'
 
-def repack(files, **kwds):
+def repack(files, remove_source=False, dryrun=False, **kwds):
     for src_path in expand_paths(files):
+        if src_path.suffix.lower() not in SUPPORTED_FILE_EXTENSIONS:
+            logger.debug(f'Skipping {src_path} which is not a supported archive format')
+            continue
+
         dst_path = src_path.with_suffix(_repack_file_type)
+
+        if src_path == dst_path:
+            continue
+
+        if dryrun:
+            logger.info(f'dryrun: {src_path} -> {dst_path}')
+            continue
 
         if dst_path.exists():
             logger.error(f'{dst_path}: already exists!')
@@ -18,6 +30,11 @@ def repack(files, **kwds):
         dst_cfile = ComicArchive(dst_path)
 
         for member in src_cfile.list():
+            if member.is_dir():
+                continue
             dst_cfile.write(member.name, src_cfile.read(member.name))
+
+        if remove_source:
+            src_path.unlink()
 
         logger.debug(f'repack complete: {src_path} -> {dst_path}')
