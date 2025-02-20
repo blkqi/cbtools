f79d738f6a81453d23a19a9cfd580b48abb882d1
ckleinschmidt <ckleinschmidt@unicef.org>
2025-02-20 08:25:33 -0500

    Add custom exception types

---
+++ src/cbtools/exceptions.py
@@ -0,0 +1,23 @@
+class CbtoolsError(Exception):
+    """Base class for all exceptions raised by cbtools."""
+    pass
+
+class InvalidArgumentError(CbtoolsError):
+    """Raised when the arguments passed to a function are invalid."""
+    pass
+
+class ParseError(CbtoolsError):
+    """Raised when a parsing error occurs."""
+    pass
+
+class MissingDependencyError(CbtoolsError):
+    """Raised when a required dependency is missing."""
+    pass
+
+class UnsupportedFileTypeError(CbtoolsError):
+    """Raised when an unsupported file is encountered."""
+    pass
+
+class SubprocessError(CbtoolsError):
+    """Raised when a subprocess command fails."""
+    pass
