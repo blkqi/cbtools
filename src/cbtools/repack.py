cee241a79ec5348f06ec46961cee06825ad51854
Cory <778005+corylk@users.noreply.github.com>
2026-07-03 12:59:13 -0400

    repack: fix cbr archives skipped in webp mode (#96)
    * repack: fix cbr archives skipped in webp mode
    
    * clearer intent
    
    ---------
    
    Co-authored-by: ckleinschmidt <ckleinschmidt@unicef.org>

--- src/cbtools/repack.py
+++ src/cbtools/repack.py
@@ -59,16 +59,17 @@ def repack(files, remove_source=False, dryrun=False, root=None, use_webp=False, 
         src_cfile = ComicArchive(src_path)
         webp_temp_path = False
 
-        if use_webp:
-            has_lossy = lambda m: m.name.lower().endswith(('.jpg', '.jpeg'))
+        has_lossy = lambda m: m.name.lower().endswith(('.jpg', '.jpeg'))
+        convert_webp = use_webp and src_cfile.match(has_lossy)
 
-            if not src_cfile.match(has_lossy):
-                if dst_path.suffix.lower() == _repack_file_type:
-                    logger.info(f'Skipping {src_path}: no jpg images found for webp conversion')
-                    continue
-            elif src_path == dst_path:
-                dst_path = src_path.with_name(dst_path.stem + '_webp' + dst_path.suffix)
-                webp_temp_path = True
+        if use_webp and not convert_webp:
+            if src_path == dst_path:
+                logger.info(f'Skipping {src_path}: no jpg images found for webp conversion')
+                continue
+
+        if convert_webp and src_path == dst_path:
+            dst_path = src_path.with_name(dst_path.stem + '_webp' + dst_path.suffix)
+            webp_temp_path = True
 
         if src_path != dst_path and dst_path.exists():
             logger.warning(f'{dst_path}: already exists!')
@@ -86,7 +87,7 @@ def repack(files, remove_source=False, dryrun=False, root=None, use_webp=False, 
             tmp_path = Path(tmpdir)
             src_cfile.extract_all(out_path=tmp_path)
 
-            if use_webp:
+            if convert_webp:
                 _batch_convert_to_webp(tmp_path)
 
             files = [str(p) for p in tmp_path.iterdir()]
